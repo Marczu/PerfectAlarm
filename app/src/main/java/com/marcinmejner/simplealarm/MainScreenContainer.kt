b8d177de52fb55559cc1c7b7ce6cff4322fae106
Marcin Mejner <marczu09@gmail.com>
2018-08-08 15:59:21 +0200

    Working fragment with bottomNavView

--- app/src/main/java/com/marcinmejner/simplealarm/MainScreenContainer.kt
+++ app/src/main/java/com/marcinmejner/simplealarm/MainScreenContainer.kt
@@ -3,7 +3,10 @@ package com.marcinmejner.simplealarm
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.view.MenuItem
+import com.marcinmejner.simplealarm.Alarm.AlarmsFragment
+import com.marcinmejner.simplealarm.Stoper.StoperFragment
 import kotlinx.android.synthetic.main.activity_main_screen.*
 
 class MainScreenContainer : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -14,6 +17,15 @@ class MainScreenContainer : AppCompatActivity(), BottomNavigationView.OnNavigati
         setContentView(R.layout.activity_main_screen)
 
         initBottomNavView()
+        displayFragment(AlarmsFragment())
+    }
+
+    /*Displaying fragment selected by botNavView*/
+    private fun displayFragment(fragment: Fragment) {
+        val fm = supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit()
     }
 
     private fun initBottomNavView() {
@@ -21,7 +33,17 @@ class MainScreenContainer : AppCompatActivity(), BottomNavigationView.OnNavigati
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment: Fragment? = null
+        when (item.itemId){
+            R.id.alarm_menu -> fragment = AlarmsFragment()
+            R.id.stoper_menu -> fragment = StoperFragment()
+        }
 
-        return false
+        if(fragment != null) {
+            displayFragment(fragment)
+        }
+        return true
     }
+
+
 }
---
+++ app/src/main/java/com/marcinmejner/simplealarm/utils/SectionPagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.marcinmejner.simplealarm.utils
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import java.util.ArrayList
+
+class SectionPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    private val TAG = "SectionPagerAdapter"
+
+    private val mFragmentList = ArrayList<Fragment>()
+
+    override fun getItem(position: Int): Fragment {
+        return mFragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    fun addFragment(fragment: Fragment) {
+        mFragmentList.add(fragment)
+    }
+}
