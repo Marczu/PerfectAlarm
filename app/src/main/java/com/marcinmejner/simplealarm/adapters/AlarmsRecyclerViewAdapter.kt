823395a526e55972c2516a88ab5d6e32a303403a
Marcin Mejner <marczu09@gmail.com>
2018-08-10 00:10:12 +0200

    fixing alarm_list_item layout

--- app/src/main/java/com/marcinmejner/simplealarm/adapters/AlarmsRecyclerViewAdapter.kt
+++ app/src/main/java/com/marcinmejner/simplealarm/adapters/AlarmsRecyclerViewAdapter.kt
@@ -8,12 +8,11 @@ import android.view.ViewGroup
 import android.view.View
 import com.marcinmejner.simplealarm.R
 import com.marcinmejner.simplealarm.model.AlarmEntity
-import kotlinx.android.synthetic.main.alarms_list_item.view.*
+import kotlinx.android.synthetic.main.alarm_list_item.view.*
 
 
 class AlarmsRecyclerViewAdapter(val alarmsList: ArrayList<AlarmEntity>, val context: Context) : RecyclerView.Adapter<AlarmsRecyclerViewAdapter.ViewHolder>() {
     private val TAG = "AlarmsRecyclerViewAdapt"
-    
 
     init {
         Log.d(TAG, "init RV tytuł to: ${alarmsList[1].name}: ")
@@ -30,15 +29,13 @@ class AlarmsRecyclerViewAdapter(val alarmsList: ArrayList<AlarmEntity>, val cont
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.alarmTitle.text = alarmsList[position]?.name
-        holder.alarmTime.text = "${alarmsList[position]?.alarmHours} : ${alarmsList[position]?.alarmMinutes}"
+        holder.alarmTime.text = "${alarmsList[position]?.alarmHours}:${alarmsList[position]?.alarmMinutes}"
         holder.alarmSnoozeTime.text = alarmsList[position]?.snoozeMinutes.toString()
 
         Log.d(TAG, "onBindViewHolder:  wewnątrz RV ${alarmsList[position]?.name}")
-
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
         val alarmItemLayout = view.alarm_item_rel_layout
         val deleteAlarm = view.alarm_item_delete_alarm
         val alarmSwitch = view.alarm_item_switch
