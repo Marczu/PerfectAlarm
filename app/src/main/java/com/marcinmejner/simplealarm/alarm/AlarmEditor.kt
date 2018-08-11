0ad716327676b8e26efdc02bff1bc5932b7f5cae
Marcin Mejner <marczu09@gmail.com>
2018-08-12 00:13:03 +0200

    Work on activity_alarm_editor layout - add numberPicker in layout and AlarmEditor

--- app/src/main/java/com/marcinmejner/simplealarm/alarm/AlarmEditor.kt
+++ app/src/main/java/com/marcinmejner/simplealarm/alarm/AlarmEditor.kt
@@ -3,10 +3,17 @@ package com.marcinmejner.simplealarm.alarm
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
 import com.marcinmejner.simplealarm.R
+import kotlinx.android.synthetic.main.activity_alarm_editor.*
+import java.util.*
 
 class AlarmEditor : AppCompatActivity() {
     private val TAG = "AlarmEditor"
+
+    //widgets
+    lateinit var hourPicker: NumberPicker
+    lateinit var minutePicker: NumberPicker
 
     //vars
     lateinit var editorViewModel: AlarmEditorViewModel
@@ -21,6 +28,40 @@ class AlarmEditor : AppCompatActivity() {
     private fun init() {
         initViewModel()
         saveNewAlarm()
+        initTimePicker()
+    }
+
+    private fun initTimePicker() {
+
+        /*Setting hour values 00 to 23*/
+        val displaydHourValues = arrayOfNulls<String>(24)
+        for (i in 0..23) {
+            if (i<10){
+                displaydHourValues[i] = "0$i"
+            }else{
+                displaydHourValues[i] = "$i"
+            }
+        }
+
+        /*Setting minutes values 00 to 60*/
+        val displayMinutesdValues = arrayOfNulls<String>(61)
+        for (i in 0..60) {
+            if (i<10){
+                displayMinutesdValues[i] = "0$i"
+            }else{
+                displayMinutesdValues[i] = "$i"
+            }
+        }
+
+        hourPicker = numberHourPicker
+        hourPicker.displayedValues = displaydHourValues
+        hourPicker.minValue = 0
+        hourPicker.maxValue = 23
+
+        minutePicker = numberMinutePicker
+        minutePicker.displayedValues = displayMinutesdValues
+        minutePicker.minValue = 0
+        minutePicker.maxValue = 60
     }
 
     private fun initViewModel() {
