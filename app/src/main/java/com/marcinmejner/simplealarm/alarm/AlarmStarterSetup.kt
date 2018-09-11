85ce96be79e3e45955af1c6c1b3f6d09c1e01101
Marcin Mejner <marczu09@gmail.com>
2018-09-12 01:30:32 +0200

    Fixed bugs, working AlarmStarterSetup and Broadcast

--- app/src/main/java/com/marcinmejner/simplealarm/alarm/AlarmStarterSetup.kt
+++ app/src/main/java/com/marcinmejner/simplealarm/alarm/AlarmStarterSetup.kt
@@ -16,8 +16,8 @@ class AlarmStarterSetup {
     //vars
     var currentAlarms: MutableList<AlarmEntity> = ArrayList()
     lateinit var alarmManager: AlarmManager
-    lateinit var pi:PendingIntent
-    var isTommorow: Boolean = false
+    lateinit var pi: PendingIntent
+    var startToday: Boolean = false
     var requestCode: Int = 0
     var pendingIntentsList = mutableListOf<PendingIntent>()
 
@@ -27,7 +27,7 @@ class AlarmStarterSetup {
         turnedOnAlarmsList.clear()
 
         turnedOnAlarms.forEach {
-            if(it.isAlarmEnabled){
+            if (it.isAlarmEnabled) {
                 turnedOnAlarmsList.add(it)
             }
         }
@@ -38,53 +38,58 @@ class AlarmStarterSetup {
         }
 
         alarmsStarter(context)
-        Log.d(TAG, "getCurrentAlarms: ${currentAlarms[0].name}")
     }
 
     /*  Iterate all alarm and set on with days alarm will repeat*/
-    fun alarmsStarter(context: Context){
+    fun alarmsStarter(context: Context) {
         alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        if(pendingIntentsList.isNotEmpty())
+        if (pendingIntentsList.isNotEmpty())
             pendingIntentsList.forEach { alarmManager.cancel(it) }
 
-        currentAlarms.forEach { alarm->
-            when {
-                alarm.mondayCheck -> setAlarm(context, 2, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
-                alarm.tuesdayCheck -> setAlarm(context,3, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
-                alarm.wednesdayCheck -> setAlarm(context,4, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
-                alarm.thursdayCheck -> setAlarm(context, 5, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
-                alarm.fridayCheck -> setAlarm(context, 6, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
-                alarm.saturdayCheck -> setAlarm(context, 7, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
-                alarm.sundayCheck -> setAlarm(context, 1, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
-            }
+        currentAlarms.forEach { alarm ->
+            if (alarm.mondayCheck) setAlarm(context, 2, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
+            if (alarm.tuesdayCheck) setAlarm(context, 3, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
+            if (alarm.wednesdayCheck) setAlarm(context, 4, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
+            if (alarm.thursdayCheck) setAlarm(context, 5, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
+            if (alarm.fridayCheck) setAlarm(context, 6, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
+            if (alarm.saturdayCheck) setAlarm(context, 7, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
+            if (alarm.sundayCheck) setAlarm(context, 1, alarm.alarmHours!!.toInt(), alarm.alarmMinutes!!.toInt(), alarm.id)
         }
     }
 
-    fun setAlarm(context: Context, weekday: Int, hours: Int, minutes: Int, id: Int) {
 
+    fun setAlarm(context: Context, weekday: Int, hours: Int, minutes: Int, id: Int) {
         val hour = hours
         val minute = minutes
-        Log.d(TAG, "setAlarm: hour: $hour")
-        Log.d(TAG, "setAlarm: minute: $minute")
+        Log.d(TAG, "setAlarm: hour: $hour , minuta $minute")
 
         val calendar = Calendar.getInstance().apply {
             set(Calendar.DAY_OF_WEEK, weekday)
             set(Calendar.HOUR_OF_DAY, hour)
             set(Calendar.MINUTE, minute)
-            set(Calendar.SECOND, 2)
+            set(Calendar.SECOND, 15)
         }
 
         var startUpTime = calendar.timeInMillis
         if (System.currentTimeMillis() > startUpTime) {
-            startUpTime += 24 * 60 * 60 * 1000
-            isTommorow = true
-        }else{
-            isTommorow = false
+            startUpTime += 7 * 24 * 60 * 60 * 1000
         }
+
+        val calendarTest = Calendar.getInstance()
+        calendarTest.timeInMillis = startUpTime
+
+        val mMinute = calendarTest.get(Calendar.MINUTE)
+        val mhour = calendarTest.get(Calendar.HOUR)
+        val mMonth = calendarTest.get(Calendar.MONTH)
+        val mDay = calendarTest.get(Calendar.DAY_OF_MONTH)
+        Log.d(TAG, "setAlarm: minuta: $mMinute")
+        Log.d(TAG, "setAlarm: godzina: $mhour")
+        Log.d(TAG, "setAlarm: miesiac: $mMonth")
+        Log.d(TAG, "setAlarm: dzien miesiaca: $mDay")
+
 
         val intent = Intent(context, MyBroadcastReciver::class.java)
         intent.putExtra(context.getString(R.string.id), id)
-        intent.putExtra(context.getString(R.string.day_of_week), weekday)
         intent.action = "com.marcinmejner.alarmmanager"
 
         pi = PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT)
@@ -92,7 +97,7 @@ class AlarmStarterSetup {
         pendingIntentsList.add(pi)
 
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, startUpTime,
-                AlarmManager.INTERVAL_DAY, pi)
+                AlarmManager.INTERVAL_DAY * 7, pi)
     }
 
 }
--- app/src/main/java/com/marcinmejner/simplealarm/broadcast/MyBroadcastReciver.kt
+++ app/src/main/java/com/marcinmejner/simplealarm/broadcast/MyBroadcastReciver.kt
@@ -5,35 +5,52 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.support.v4.content.ContextCompat.startActivity
+import com.marcinmejner.simplealarm.R
 import com.marcinmejner.simplealarm.alarm.AlarmRingingScreen
 import com.marcinmejner.simplealarm.alarm.AlarmStarterSetup
+import java.time.DayOfWeek
+import java.util.*
 
-class MyBroadcastReciver: BroadcastReceiver(){
+class MyBroadcastReciver : BroadcastReceiver() {
     private val TAG = "MyBroadcastReciver"
 
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.d(TAG, "onReceive: inside onRecive")
 
-        if(intent?.action.equals("com.marcinmejner.alarmmanager")){
-            var b = intent?.extras
-            Log.d(TAG, "onReceive: alarm started")
-            initAlarmActivity(context)
-        }
-        else if(intent?.action.equals("android.permission.RECEIVE_BOOT_COMPLETED")){
+        if (intent?.action.equals("com.marcinmejner.alarmmanager")) {
+            var extras = intent?.extras
+            val id = extras?.getInt("id")
+            val weekday = extras?.getInt("day_of_week")
+            val timeInMilis = extras?.getLong("time_in_millis")
+            val startToday = extras?.getBoolean("start_today")
+            Log.d(TAG, "onReceive: alarm started, id: $id, weekday: $weekday")
+            initAlarmActivity(context, id, weekday, startToday)
+        } else if (intent?.action.equals("android.permission.RECEIVE_BOOT_COMPLETED")) {
             val alarmStarterSetup = AlarmStarterSetup()
             alarmStarterSetup.alarmsStarter(context!!)
         }
     }
 
     /*Go to AlarmRingingScreen*/
-    fun initAlarmActivity(context: Context?){
-        Intent(context, AlarmRingingScreen::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP
-            action = "android.intent.action.VIEW"
-            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            action = Intent.ACTION_MAIN
-            addCategory(Intent.CATEGORY_LAUNCHER)
-            startActivity(context!!, this, null)
+    private fun initAlarmActivity(context: Context?, id: Int?, weekday: Int?, startToday: Boolean?) {
+//        if (canActivateAlarm(weekday, startToday)) {
+            Intent(context, AlarmRingingScreen::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP
+                action = "android.intent.action.VIEW"
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                action = Intent.ACTION_MAIN
+                addCategory(Intent.CATEGORY_LAUNCHER)
+                putExtra(context?.getString(R.string.alarm_ring_id), id)
+                startActivity(context!!, this, null)
+            }
+//        }
+    }
+
+    private fun canActivateAlarm(weekday: Int?, startToday: Boolean?): Boolean {
+        val calendar = Calendar.getInstance()
+        if (calendar.get(Calendar.DAY_OF_WEEK) == weekday && startToday!!) {
+            return true
         }
+        return false
     }
 }
